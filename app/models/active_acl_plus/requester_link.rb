33507ceff845b1697e4ca066a3c56ea58512c7de
Anay Kamat <anay@presentsoft.co.in>
2013-04-09 17:35:31 +0530

    anay: Adding required target and requester accessible fields on target_link and requester_link model

--- app/models/active_acl_plus/requester_link.rb
+++ app/models/active_acl_plus/requester_link.rb
@@ -1,10 +1,12 @@
 module ActiveAclPlus
   class RequesterLink < ActiveRecord::Base
     set_table_name ActiveAclPlus.requester_links_table
-    
+
+    attr_accessible :acl_id, :requester_id, :requester_type, :requester
+
     belongs_to :acl, :class_name => "ActiveAclPlus::Acl"
     belongs_to :requester, :polymorphic => true
-    
+
     def self.reloadable? #:nodoc:
       return false
     end
--- app/models/active_acl_plus/target_link.rb
+++ app/models/active_acl_plus/target_link.rb
@@ -1,10 +1,12 @@
 module ActiveAclPlus
   class TargetLink < ActiveRecord::Base
     set_table_name ActiveAclPlus.target_links_table
-    
+
+    attr_accessible :acl_id, :target_id, :target_type, :target
+
     belongs_to :acl, :class_name => "ActiveAclPlus::Acl"
     belongs_to :target, :polymorphic => true
-    
+
     def self.reloadable? #:nodoc:
       return false
     end
